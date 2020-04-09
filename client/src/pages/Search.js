45c2316604e2e5507d3fa7ffe7ea4ff7d8a04055
Andres Long <andreslong95@gmail.com>
2020-04-08 23:02:06 -0500

    deploying while unfinished

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,37 +1,86 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 
 function Search() {
-    const [search, setSearch] = useState({search: ""})
+    const [search, setSearch] = useState({input: ""})
+    const [currentSearch, setCurrentSearch] = useState([])
 
     function handleInputChange(event) {
         const { name, value } = event.target;
         setSearch({[name]: value})
     }
+    useEffect(() => {
+        initialLoad()
+    }, [])
+
+    function initialLoad() {
+        API.bookSearch("The Hunger Games")
+          .then(res =>
+             setCurrentSearch(res.data) 
+          )
+          .catch(err => console.log(err))
+    }
     function handleSubmit(event) {
         event.preventDefault();
         console.log(search);
-        API.bookSearch(search.search)
-          .then(res => console.log(res));
+        API.bookSearch(search.input)
+          .then(res => setCurrentSearch(res.data))
+          .then(res => console.log(currentSearch))
+          .catch(err => console.log(err))
+    }
+
+    function saveBook(event) {
+
     }
     return (
         <>
-        <div className="card">
+        <div className="container card text-center">
             <div className="card-body">
-        <form className="form-inline">
-            <div class="form-group mb-2">
-                <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Search"/>
+                <form className="form-inline">
+                    <div className="form-group mb-2">
+                        <label  className="sr-only">Search</label>
+                        <p>Search</p>
+                    </div>
+                    <div className="form-group mx-sm-3 mb-2">
+                        <label  className="sr-only">Search</label>
+                        <input type="text" name="input" onChange={handleInputChange} className="form-control form-control-lg" id="inputPassword2" placeholder="Search"/>
+                    </div>
+                    <button type="submit" onClick={handleSubmit} className="btn btn-primary mb-2">Search</button>
+                </form>
             </div>
-            <div class="form-group mx-sm-3 mb-2">
-                <label  class="sr-only">Search</label>
-                <input type="text" name="search" onChange={handleInputChange} class="form-control" id="inputPassword2" placeholder="Search"/>
+        </div>
+        <div>
+        {currentSearch.length ? (
+        <div>
+        {currentSearch.items.map(element => {
+            return (
+                <div className="card container mt-5">
+            <div className="card-body col-lg-12">
+                <h5 className="card-title">{element.volumeInfo.title}</h5>
+                {/* Author Image Save Link */}
+                <h6 className="card-subtitle mb-2 text-muted">{element.volumeInfo.authors[0]}</h6>
+                <div className="row">
+                <div className="col-lg-2">
+                    <img src=""/>
+                </div>
+                <div className="col-lg-8">
+                    <p className="card-text">{element.volumeInfo.description}</p>
+                </div>
+                <div className="col-lg-2">
+                    <button className="btn btn-danger">Add to Favorites</button>
+                </div>
+                </div>
+                <a href={element.selfLink} className="card-link">Link to book</a>
             </div>
-            <button type="submit" onClick={handleSubmit} class="btn btn-primary mb-2">Search</button>
-        </form>
+            </div>
+            )  
+        })}
         </div>
-        </div>
-        </>
+        ) : (
+            <h3 className="text-center"> Search for some books!</h3>
+        )}
+    </div>
+    </>
     )
 }
 
